4f5facab3984a2fbc4dd455e3103798682e16052
yusufuysal <yusuf.uysal@agu.edu.tr>
2023-12-31 04:24:27 +0300

    Add popular movies card list

---
+++ src/data/popularMovies.js
@@ -0,0 +1,44 @@
+export const popularMovies = [
+  {
+    title: "Poor Things (2023)",
+    image:
+      "https://a.ltrbxd.com/resized/film-poster/7/1/0/3/5/2/710352-poor-things-0-230-0-345-crop.jpg?v=a0f2ee9a0e",
+    view: 134.98,
+    like: 59.482,
+  },
+  {
+    title: "The Boy and the Heron (2023)",
+    image:
+      "https://a.ltrbxd.com/resized/film-poster/4/3/8/6/9/2/438692-the-boy-and-the-heron-0-150-0-225-crop.jpg?v=521e114164",
+    view: 277.792,
+    like: 107.785,
+  },
+  {
+    title: "Saltburn (2023)",
+    image:
+      "https://a.ltrbxd.com/resized/film-poster/8/3/5/7/7/4/835774-saltburn-0-150-0-225-crop.jpg?v=b58cbd3b08",
+    view: 637.483,
+    like: 199.396,
+  },
+  {
+    title: "Fallen Leaves (2023)",
+    image:
+      "https://a.ltrbxd.com/resized/film-poster/8/8/8/0/5/4/888054-fallen-leaves-0-150-0-225-crop.jpg?v=0726b99f09",
+    view: 49.98,
+    like: 17.883,
+  },
+  {
+    title: "May December (2023)",
+    image:
+      "https://a.ltrbxd.com/resized/sm/upload/zh/p4/cg/6v/may-december-0-150-0-225-crop.jpg?v=40f7f230a3",
+    view: 270.394,
+    like: 77.657,
+  },
+  {
+    title: "Monster (2023)",
+    image:
+      "https://a.ltrbxd.com/resized/film-poster/9/4/7/0/3/0/947030-monster-0-150-0-225-crop.jpg?v=9787f75532",
+    view: 37.873,
+    like: 16.938,
+  },
+];
